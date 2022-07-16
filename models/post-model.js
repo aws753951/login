cb7dd267f95d14a09877c54feada17144e1b5556
Marc <a0983204766@gmail.com>
2022-07-16 11:45:11 +0800

    add poster message to profile

---
+++ models/post-model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  ID: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Poster = require("../models/post-model");
 
 const checkAuthentication = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -8,8 +9,32 @@ const checkAuthentication = (req, res, next) => {
   }
 };
 
-router.get("/", checkAuthentication, (req, res) => {
-  res.render("profile", { user: req.user });
+router.get("/", checkAuthentication, async (req, res) => {
+  let posts = await Poster.find({ ID: req.user._id });
+  res.render("profile", { user: req.user, posts });
+});
+
+router.get("/post", checkAuthentication, (req, res) => {
+  res.render("post", { user: req.user });
+});
+
+router.post("/post", checkAuthentication, (req, res) => {
+  let { title, content } = req.body;
+  let newPost = new Poster({
+    title,
+    content,
+    username: req.user.name,
+    ID: req.user._id,
+  });
+  newPost
+    .save()
+    .then(() => {
+      res.status(200).redirect("/profile");
+    })
+    .catch((e) => {
+      req.flash("error_msg", "Both title and contenct required.");
+      res.redirect("/profile/post");
+    });
 });
 
 module.exports = router;
